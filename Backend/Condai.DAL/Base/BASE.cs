88abe3d67550a4b86a64c42a57f2f40bf7130709
Bryan Alejandro Cubillos Prieto <bryant_alejandro@hotmail.com>
2017-05-30 16:32:02 -0500

    Base connections

---
+++ Backend/Condai.DAL/Base/BASE.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace Condai.DAL
+{
+    public class BASE
+    {
+        #region [ Attribute ]
+
+        private static BASE instance;
+
+        #endregion
+
+        #region [ Constructor ]
+
+        private BASE() { }
+
+        #endregion
+
+        #region [ Properties ]
+
+        public static BASE Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new BASE();
+
+                return instance;
+            }
+        }
+
+        #endregion
+
+        #region [ Methods ]
+
+
+
+        #endregion
+    }
+}
